cafbf231fabf1dd7f40a0208268425e2a6738f61
Sávio Decaro <savio.decaro@gmail.com>
2024-11-14 12:50:59 -0300

    Tira da torreta

--- MaverickGame/Assets/Prototype/Scripts/LookAtPlayer.cs
+++ MaverickGame/Assets/Prototype/Scripts/LookAtPlayer.cs
@@ -8,8 +8,16 @@ public class LookAtPlayer : MonoBehaviour
         
     }
     [SerializeField] private Transform torreta, player;
+    
     [SerializeField] private float airPlaneHeight = 10.0f;
-    [SerializeField] private GameObject projectile;
+    
+    [SerializeField] private float projectileSpeed = 10.0f;
+
+
+    [SerializeField] private GameObject muzzleFlash;
+    
+
+    
     // Update is called once per frame
 
     void Update()
@@ -19,16 +27,30 @@ public class LookAtPlayer : MonoBehaviour
            trackPlane();
             if(player.position.y >= airPlaneHeight){
                 shootPlane();
+                muzzleFlash.SetActive(true);
+
+            }
+            else
+            {
+                muzzleFlash.SetActive(false);
+
             }
         }
     }
 
     void trackPlane(){
         torreta.transform.LookAt(player.position);
+        
     }
     
-    void shootPlane(){
-        Debug.Log("Disparando no Avião");
-        Instantiate(projectile);
+    void shootPlane()
+    {
+        Debug.Log("atirando");
+        GameObject launchedObject = Instantiate(projectile, transform.position, transform.rotation);
+        launchedObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, projectileSpeed, 0));
+        
     }
+
+    
+    
 }
